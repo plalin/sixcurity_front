7d5de3273f96faf6d7a392f503c6270a38aa04cd
lego1404 <lego1404@korea.ac.kr>
2022-11-02 21:18:19 +0900

    11/02 Mainpage added

--- Component/Header/Header.svelte
+++ Component/Header/Header.svelte
@@ -2,11 +2,10 @@
 <nav>
   <a href="#/">Home</a>
   <a href="#/Contact1">Contact1</a>
-  <!-- App컴포넌트에서 '/Contact2/:test' 여기의 :test
-  		가 1234가 된다.-->
   <a href="#/Contact2/1234">Contact2</a>
   <a href="#/Login">Login</a>
   <a href="#/SignIn">SignIn</a>
+  <a href="#/Mainpage">Mainpage</a>
   <br><br><br><br>
 </nav>
 
